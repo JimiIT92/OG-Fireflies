e7db6b5c44be7b85ee65ca2c37e09ae9758769da
JimiIT92 <jimiit92@gmail.com>
2025-03-25 09:48:45 +0100

    Updated to 1.21.5

---
+++ build.ps1
@@ -0,0 +1,12 @@
+#Variables
+$VERSION = "1.0"
+$MC_VERSION = "1.21.5"
+$NAME = "OG Fireflies"
+$PACK_EXTENSION = "v"+$VERSION+"-"+$MC_VERSION+".zip"
+$RESOURCE_PACK_PATH = $NAME+"-"+$PACK_EXTENSION
+
+Remove-Item $RESOURCE_PACK_PATH -Force
+
+Compress-Archive -force "./assets" $RESOURCE_PACK_PATH
+Compress-Archive -update "pack.mcmeta" $RESOURCE_PACK_PATH
+Compress-Archive -update "pack.png" $RESOURCE_PACK_PATH
